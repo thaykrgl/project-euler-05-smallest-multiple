0e93d7b165802f1c447b1a7348425a8ae6eeaae4
Taha <35704393+thaykrgl@users.noreply.github.com>
2022-06-23 20:08:31 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ const ekok = (sayi1, sayi2) => {
     return ((sayi1) / ebob(sayi1, sayi2)) * sayi2;
 }
 
-// Belirlemiş olduğumuz değere kadar devam edecek olan for döngümüzle beraber ikişerli şekilde ekok hesaplayarak 
+/* Belirlemiş olduğumuz değere kadar devam edecek olan for döngümüzle beraber ikişerli şekilde ekok hesaplayarak 
 ve bulduğumuz sonucu bir sonraki iterasyona devrederek sürekli büyüyen bir sonuç elde ediyoruz. n değerine ulaştığımızda da 
 artık elimizde n değerine kadar olan sayıların en küçük ortak katı bulunuyor.
+*/
 const smallestMultiple = n => {
         let result = 1;
         for (let i = 1; i <= n; i++) {
